634efbb157f5ff59c5f6356d512978d4e2722427
zhuhaojie <zhuhaojie@cqslim.com>
2025-01-21 13:43:31 +0800

    feat: eslint

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueRouter from 'unplugin-vue-router/vite'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    VueRouter({
+      extensions: ['.vue'],
+      routesFolder: 'src/pages',
+      exclude: ['src/pages/**/components/**'],
+      dts: false,
+    }),
+    vue(),
+  ],
 })
